5c530332bdb97809db7951bfa39db6d3cd7abd68
Dan Haddock <dhbuild.002@gmail.com>
2020-11-11 20:55:26 +0000

    Final Touches to adding and removing from a Checkout page - Using a splice Array

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,12 @@
 //import { ShoppingBasket } from "@material-ui/icons";
 import React from "react";
 import "./Checkout.css";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
+import CheckoutProduct from './CheckoutProduct'; 
 
-function Checkout() {
+function Checkout(){
+  const [{ basket }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -13,23 +16,24 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
+        <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          {/*checkoutProduct*/}
-          {/*checkoutProduct*/}
-          {/*checkoutProduct*/}
-          {/*checkoutProduct*/}
-          {/*checkoutProduct*/}
+            {basket.map(item => (
+               <CheckoutProduct 
+                id={item.id} 
+                title={item.title} 
+                image={item.image} 
+                price={item.price} 
+                rating={item.rating}
+                /> 
+            ))} 
+       </div>
        </div>
 
       <div className="checkout__right">
         <Subtotal />
       </div>
-
-     
-      
-    
-      
-      </div>    
+      </div>        
   );
 }
 
