b48418299f96b6ededf30138f7ce8c86808056be
Dan Haddock <dhbuild.002@gmail.com>
2020-11-21 17:21:43 +0000

    Add in ability to see username in header and checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,10 +3,10 @@ import React from "react";
 import "./Checkout.css";
 import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
-import CheckoutProduct from './CheckoutProduct';
+import CheckoutProduct from "./CheckoutProduct";
 
-function Checkout(){
-  const [{ basket }, user,  dispatch] = useStateValue();
+function Checkout() {
+  const [{ basket, user}, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -17,24 +17,24 @@ function Checkout(){
           alt=""
         />
         <div>
-  <h3>Hello, {user?.email}</h3>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-            {basket.map(item => (
-               <CheckoutProduct 
-                id={item.id} 
-                title={item.title} 
-                image={item.image} 
-                price={item.price} 
-                rating={item.rating}
-                /> 
-            ))} 
-       </div>
-       </div>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
+      </div>
 
       <div className="checkout__right">
         <Subtotal />
       </div>
-      </div>        
+    </div>
   );
 }
 
